2f6542b56625e8b44e169d3d68f9380bc1e95754
honzikhk <chudicekjan@gmail.com>
2024-01-07 06:06:14 +0100

    new model, migrations

--- precious_metal/models.py
+++ precious_metal/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class PreciousMetal(models.Model):
+    # id field is added automatically
+    item = models.CharField(max_length=30)
+    value = models.IntegerField()
+    note = models.CharField(null=True, blank=True, max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.item        # represent objects by their names in admin page
