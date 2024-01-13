a1f21d50ce1fec567462d1320576fe3151d0242b
honzikhk <chudicekjan@gmail.com>
2024-01-13 08:07:44 +0100

    for loop in homepage_precious_metal template

--- precious_metal/views.py
+++ precious_metal/views.py
@@ -2,14 +2,14 @@ from django.views.generic import ListView, CreateView
 from .models import PreciousMetal
 from precious_metal.forms import MetalForm
 
-class PreciousMetalHomepage(ListView):
+class PreciousMetalHomepage(ListView): #Listview has probably "objectlist" as original function
     model = PreciousMetal
-    template_name = "precious_metal/homepage_precious_metal.html" #try to remove "precious_metal/"
+    template_name = "precious_metal/homepage_precious_metal.html" 
 
 
 class CreateMetal(CreateView):
     # after click on "submit" it redirect to "precious_metal_homepage" because of the method in model "get_absolute_url"
-    template_name = "precious_metal/metal_create.html"  #try to remove "precious_metal/"
+    template_name = "precious_metal/metal_create.html"
     form_class = MetalForm
     model = PreciousMetal
     
