16512474c904de9cd3b8851796b884515c429e8b
honzikhk <chudicekjan@gmail.com>
2024-01-16 10:18:54 +0100

    create functions to sum values in base/views.py

--- base/views.py
+++ base/views.py
@@ -1,5 +1,22 @@
 from django.views.generic import TemplateView
+from precious_metal.models import PreciousMetal
+from django.template.response import TemplateResponse
 
 
 class BaseHomepageView(TemplateView):
     template_name = "base/base_homepage.html"
+
+    def count_sum_of_values(self):      # sum of all values of all metals in USD
+        sum = 0
+        for metal in PreciousMetal.objects.all():
+            sum += metal.value
+        return sum
+
+    def get(self, request, *args, **kwargs):    # maybe remove *args, **kwargs
+        context = {
+            "count_of_items": PreciousMetal.objects.all().count(),      # shows how many items is in precoius metals
+            "sum_of_values": self.count_sum_of_values(),
+            'page_name': 'Books homepage'   # wtf is that
+        }
+        return TemplateResponse(request, "base/base_homepage.html", context=context)
+
