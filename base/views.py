3b3d49266f36c29e422cec991a8869008bd884ea
honzikhk <chudicekjan@gmail.com>
2024-01-20 07:24:04 +0100

    summary of RE on homepage

--- base/views.py
+++ base/views.py
@@ -1,22 +1,30 @@
 from django.views.generic import TemplateView
 from precious_metal.models import PreciousMetal
+from real_estates.models import RealEstate
 from django.template.response import TemplateResponse
 
 
 class BaseHomepageView(TemplateView):
     template_name = "base/base_homepage.html"
 
-    def count_sum_of_values(self):      # sum of all values of all metals in USD
+    def count_sum_of_values_precious_metals(self):      # sum of all values of all metals in USD
         sum = 0
         for metal in PreciousMetal.objects.all():
             sum += metal.value
         return sum
 
+    def count_sum_of_values_real_estates(self):
+        sum = 0
+        for real_estate in RealEstate.objects.all():
+            sum += real_estate.value
+        return sum
+
     def get(self, request, *args, **kwargs):    # maybe remove *args, **kwargs
         context = {
-            "count_of_items": PreciousMetal.objects.all().count(),      # shows how many items is in precoius metals
-            "sum_of_values": self.count_sum_of_values(),
-            'page_name': 'Books homepage'   # wtf is that
+            "precious_metals_count_of_items": PreciousMetal.objects.all().count(),      # shows how many items is in precoius metals
+            "precious_metals_sum_of_values": self.count_sum_of_values_precious_metals(),
+            "real_estates_count_of_items": RealEstate.objects.all().count(),        # shows how many items is in real estates
+            "real_estates_sum_of_values": self.count_sum_of_values_real_estates(),
         }
         return TemplateResponse(request, "base/base_homepage.html", context=context)
 
